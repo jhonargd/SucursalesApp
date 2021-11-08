06a574722c1a207e9ce725eaaa1a2b4e9db4546a
j-h-o <ing.jhonarg@gmail.com>
2021-11-08 18:53:21 -0500

    Proyecto SucursalesApp sprint 2

---
+++ app/src/main/java/com/mintic/sucursalesapp/ListaDireccionesActivity.java
@@ -0,0 +1,14 @@
+package com.mintic.sucursalesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaDireccionesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_direcciones);
+    }
+}
